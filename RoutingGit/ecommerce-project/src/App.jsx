10e59c0a3ef29f3faf3d5b09f4dac13fed22988e
poromvp <quangkietle382@gmail.com>
2026-07-20 15:17:01 +0700

    Lesson 6 - Exercise H

--- RoutingGit/ecommerce-project/src/App.jsx
+++ RoutingGit/ecommerce-project/src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router'
 import { HomePage } from './pages/HomePage'
 import { CheckoutPage } from './pages/checkout/CheckoutPage'
 import { OrdersPage } from './pages/OrdersPage'
+import { Tracking } from './pages/Tracking'
 
 import './App.css'
 
@@ -12,6 +13,7 @@ function App() {
       <Route index element={<HomePage/>} />
       <Route path="checkout" element={<CheckoutPage/>} />
       <Route path="orders" element={<OrdersPage/>} />
+      <Route path="tracking" element={<Tracking/>} />
     </Routes>
   )
 }
