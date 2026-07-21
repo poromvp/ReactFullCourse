07865bb6d5345290bfec4f1cc5022642b59aef9e
poromvp <quangkietle382@gmail.com>
2026-07-22 00:08:44 +0700

    Seperate into smaller components

--- ReactWithBE_DataFetching_LoadData/ecommerce-project/src/App.jsx
+++ ReactWithBE_DataFetching_LoadData/ecommerce-project/src/App.jsx
@@ -1,7 +1,7 @@
 import { Routes, Route } from 'react-router'
 import { HomePage } from './pages/home/HomePage'
 import { CheckoutPage } from './pages/checkout/CheckoutPage'
-import { OrdersPage } from './pages/OrdersPage'
+import { OrdersPage } from './pages/orders/OrdersPage'
 import { Tracking } from './pages/Tracking'
 import { NotFoundPage } from './pages/NotFoundPage'
 import { useEffect, useState } from 'react'
