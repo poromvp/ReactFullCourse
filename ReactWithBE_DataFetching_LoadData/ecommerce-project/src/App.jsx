60ead833815981dd48cd3ba1bc7fe3eaaf59f26a
poromvp <quangkietle382@gmail.com>
2026-07-23 19:19:39 +0700

    Lesson 7 - Exercise H

--- ReactWithBE_DataFetching_LoadData/ecommerce-project/src/App.jsx
+++ ReactWithBE_DataFetching_LoadData/ecommerce-project/src/App.jsx
@@ -24,7 +24,7 @@ function App() {
       <Route index element={<HomePage cart={cart} />} />
       <Route path="checkout" element={<CheckoutPage cart={cart} />} />
       <Route path="orders" element={<OrdersPage cart={cart} />} />
-      <Route path="tracking" element={<Tracking />} />
+      <Route path="tracking" element={<Tracking cart={cart}/>} />
       <Route path="*" element={<NotFoundPage />} />
     </Routes>
   )
