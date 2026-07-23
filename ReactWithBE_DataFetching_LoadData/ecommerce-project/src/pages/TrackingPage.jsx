2e02128d9637b5b02d43560e94519646c9fe1e14
poromvp <quangkietle382@gmail.com>
2026-07-23 23:50:03 +0700

    Lesson 7 - Exercise N

--- ReactWithBE_DataFetching_LoadData/ecommerce-project/src/pages/TrackingPage.jsx
+++ ReactWithBE_DataFetching_LoadData/ecommerce-project/src/pages/TrackingPage.jsx
@@ -33,6 +33,11 @@ export function Tracking({cart}) {
         deliveryPercent = 100;
     }
 
+    const isPreparing = deliveryPercent < 33;
+    const isShipped = deliveryPercent >= 33 && deliveryPercent < 100;
+    const isDelivered = deliveryPercent === 100;
+
+
     return (
         <>
                 <title>Tracking</title>
@@ -63,13 +68,13 @@ export function Tracking({cart}) {
                     <img className="product-image" src={orderProduct.product.image} />
 
                     <div className="progress-labels-container">
-                        <div className="progress-label">
+                        <div className={`progress-label ${isPreparing && 'current-status'}`}>
                             Preparing
                         </div>
-                        <div className="progress-label current-status">
+                        <div className={`progress-label ${isShipped && 'current-status'}`}>
                             Shipped
                         </div>
-                        <div className="progress-label">
+                        <div className={`progress-label ${isDelivered && 'current-status'}`}>
                             Delivered
                         </div>
                     </div>
